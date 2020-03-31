65a7e890cfccaee46f0f602fbc0f0cb354a25ecf
Andy Sanchez <andysanchez726@gmail.com>
2020-03-30 21:47:56 -0500

    actually added a full round

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,12 +17,26 @@ var fight = function() {
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
     );
+    //Check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.")
+    }
     //Subtract enemyAttack from playerHealth, use result to update playerHealth
     playerHealth = playerHealth - enemyAttack
     //Log resulting message to the console so we know that it worked
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remainging. "
     );
+    // Check player's health
+    if(playerHealth <= 0) {
+        window.alert(playerName + " has died!")
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.")
+    }
 };
 
 fight();
