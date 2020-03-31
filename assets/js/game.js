5f57b19aab584281a9db4b5b255d653fdce23d08
Andy Sanchez <andysanchez726@gmail.com>
2020-03-30 21:24:00 -0500

    added a round of the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,28 @@
-var playerName = window.prompt ("What is your robot's name?");
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
-function fight() {
-    window.alert("The fight has begun!");
-}
-// fight ()
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 12;
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = "50";
+var enemyAttack = "12";
+
+var fight = function() {
+    // Alert users that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    //Subtract playerAttack from enemyHealth, use result to update enemyHealth
+    enemyHealth = enemyHealth - playerAttack
+    //log resulting message to the console so we know that it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+    );
+    //Subtract enemyAttack from playerHealth, use result to update playerHealth
+    playerHealth = playerHealth - enemyAttack
+    //Log resulting message to the console so we know that it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remainging. "
+    );
+};
+
+fight();
